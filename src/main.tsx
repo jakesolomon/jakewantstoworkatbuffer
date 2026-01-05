ef9074614e4953da8b87971ed231f8fa91c576e3
Jake Solomon <jake.ch.solomon@gmail.com>
2026-01-05 12:44:34 -0500

    Cleanup redundant styling

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,6 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MantineProvider theme={theme}>
-      <style>{`body { color: rgb(15, 14, 14); }`}</style>
       <App />
     </MantineProvider>
   </StrictMode>,
