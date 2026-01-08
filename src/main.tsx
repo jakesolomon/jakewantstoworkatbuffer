e2324b28632254af80f063f055840bac660ae5d6
Jake Solomon <jake.ch.solomon@gmail.com>
2026-01-08 10:15:11 -0500

    Add logrocket

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { MantineProvider } from '@mantine/core'
 import { Analytics } from "@vercel/analytics/react"
+import LogRocket from 'logrocket';
 import '@mantine/core/styles.css'
 import '@mantine/carousel/styles.css'
 import { theme } from './theme'
 import App from './App.tsx'
+
+if (import.meta.env.PROD) {
+  LogRocket.init('j5epm2/jakewantstoworkatbuffer');
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
